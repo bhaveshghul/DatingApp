8c56bbfedfe046d5f1c478aa1c46adae2607b617
Bhavesh Ghul <bhaveshghul@rediffmail.com>
2020-01-07 11:37:31 +0530

    Sections Three Code

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options) : base(options){}
 
         public DbSet<Values> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
